4cd09a41e6cbdded0becd8dee343ce7b1310fbac
lbsamin2 <lbsamin2@up.edu.ph>
2026-09-17 22:58:49 +0800

    revised docs organization

--- docs/page.js
+++ docs/page.js
@@ -3,10 +3,17 @@ function showPage(pageId) {
       page.classList.remove('active');
    });
    
-   document.getElementById(pageId).classList.add('active');
+   const target = document.getElementById(pageId);
+   
+   if (!target) {
+      console.error("Page not found:", pageId);
+      return;
+   }
+   
+   target.classList.add("active");
    
    window.scrollTo({
       top: 0,
-      behavior: 'instant'
+      behavior: 'auto'
    });
 }
