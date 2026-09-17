e6ca1c8c21292cc63b2c72fba0913464a66caec9
lbsamin2 <lbsamin2@up.edu.ph>
2026-09-17 21:29:35 +0800

    added js file

---
+++ docs/page.js
@@ -0,0 +1,14 @@
+<script>
+   function showPage(pageId) {
+      document.querySelectorAll(".page").forEach(page => {
+         page.classList.remove('active');
+      });
+      
+      document.getElementById(pageId).classList.add('active');
+      
+      window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+      });
+   }
+</script>
