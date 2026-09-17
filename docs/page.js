a088c8625bbfa200262b6a3dd6c6278b31c0e496
lbsamin2 <lbsamin2@up.edu.ph>
2026-09-17 22:15:11 +0800

    revised page.js

--- docs/page.js
+++ docs/page.js
@@ -1,14 +1,12 @@
-<script>
-   function showPage(pageId) {
-      document.querySelectorAll(".page").forEach(page => {
-         page.classList.remove('active');
-      });
-      
-      document.getElementById(pageId).classList.add('active');
-      
-      window.scrollTo({
-         top: 0,
-         behavior: 'instant'
-      });
-   }
-</script>
+function showPage(pageId) {
+   document.querySelectorAll(".page").forEach(page => {
+      page.classList.remove('active');
+   });
+   
+   document.getElementById(pageId).classList.add('active');
+   
+   window.scrollTo({
+      top: 0,
+      behavior: 'instant'
+   });
+}
